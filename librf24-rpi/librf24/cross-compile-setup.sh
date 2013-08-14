e09ddca197ba3dad65edf67fb7388a1fbdfe3892
Colin Cooper <cdcooperuk@googlemail.com>
2013-08-14 18:30:52 +0100

    improve/simplify
    use by . ./cross-compile-setup.sh
    make

--- librf24-rpi/librf24/cross-compile-setup.sh
+++ librf24-rpi/librf24/cross-compile-setup.sh
@@ -1,9 +1,10 @@
+tools="/home/ccooper/projects/tools"
 export ARCH=arm
 export CROSS_COMPILE=arm-bcm2708hardfp-linux-gnueabi-
-export CC=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-gcc
-export NM=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-nm
-export LD=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ld
-export CXX=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-g++
+export CC=${tools}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-gcc
+export NM=${tools}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-nm
+export LD=${tools}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ld
+export CXX=${tools}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-g++
 export CPP=$CXX
-export RANLIB=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ranlib
-export AR=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ar
+export RANLIB=${tools}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ranlib
+export AR=${tools}/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ar
