515183350ee445d8bd62a195a58a1ddd567fafd6
Colin Cooper <cdcooperuk@googlemail.com>
2013-08-14 16:02:00 +0100

    initial cross-compilation setup.
    requires tools from git://github.com/raspberrypi/tools.git

---
+++ librf24-rpi/librf24/cross-compile-setup.sh
@@ -0,0 +1,9 @@
+export ARCH=arm
+export CROSS_COMPILE=arm-bcm2708hardfp-linux-gnueabi-
+export CC=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-gcc
+export NM=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-nm
+export LD=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ld
+export CXX=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-g++
+export CPP=$CXX
+export RANLIB=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ranlib
+export AR=/home/ccooper/projects/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ar
